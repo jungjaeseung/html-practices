9d5e9938f8ec2564853ae477a4dbde964a7f1bb9
3Harang <jungjaeseung9475@gmail.com>
2023-01-25 00:28:15 +0900

    09 수정
    server에서 data list를 받아왔다 가정하고 더보기 버튼 클릭시 받은 data list를 반영하여 상품 더 보여주도록 수정
    가격낮은순 버튼 누를시 가격에따라 오름차순 정리되도록 구현
    가격낮은순 누른상태에서 더보기 버튼 누를시 정렬되서 나오도록 구현

--- js/09.js
+++ js/09.js
@@ -1,14 +1,73 @@
-var products = [
+const products = [
   { id: 0, price: 70000, title: "Blossom Dress" },
   { id: 1, price: 50000, title: "Springfield Shirt" },
   { id: 2, price: 60000, title: "Black Monastery" },
 ];
 
-for (let i = 0; i < products.length; i++) {
-  document.querySelectorAll(".card-body>h5")[
-    i
-  ].innerHTML = `${products[i]["title"]}`;
-  document.querySelectorAll(".card-body>p")[
-    i
-  ].innerHTML = `가격 : ${products[i]["price"]}`;
+let clickedCount = 0;
+let isSorted = false;
+let getUrls = [
+  "https://codingapple1.github.io/js/more1.json",
+  "https://codingapple1.github.io/js/more2.json",
+];
+
+function createForm(dataArr) {
+  dataArr.forEach(function (data) {
+    const div_layout = `<div class="col-sm-4">
+    <img src="img/product1.jpg" class="w-100" />
+    <h5>${data["title"]}</h5>
+    <p>가격 : ${data["price"]}</p>
+  </div>`;
+    $(".row").append(div_layout);
+  });
 }
+function createNewProducts(url) {
+  $.get(url).done(function (data) {
+    createForm(data);
+  });
+}
+function createSortedProducts(url) {
+  $.get(url).done(function (data) {
+    let sortedData = data.sort((a, b) => a.price - b.price);
+    createForm(sortedData);
+  });
+}
+
+$("#more").on("click", function () {
+  if (isSorted == false) {
+    if (getUrls[clickedCount] != null) {
+      createNewProducts(getUrls[clickedCount]);
+      clickedCount = clickedCount + 1;
+      if (clickedCount == getUrls.length) {
+        $("#more").html("더보기 없음");
+        $("#more").removeClass("btn-danger");
+        $("#more").addClass("btn-secondary");
+      }
+    }
+  } else {
+    if (getUrls[clickedCount] != null) {
+      createSortedProducts(getUrls[clickedCount]);
+      clickedCount = clickedCount + 1;
+      if (clickedCount == getUrls.length) {
+        $("#more").html("더보기 없음");
+        $("#more").removeClass("btn-danger");
+        $("#more").addClass("btn-secondary");
+      }
+    }
+  }
+});
+
+$("#sort-btn").on("click", function () {
+  isSorted = !isSorted;
+  $(".row").html("");
+  const sortedArr = products.sort((a, b) => a.price - b.price);
+  createForm(sortedArr);
+  for (let i = 0; i < clickedCount; i++) {
+    createSortedProducts(getUrls[i]);
+  }
+  $("#sort-btn").html("정렬완료");
+  $("#sort-btn").removeClass("btn-danger");
+  $("#sort-btn").addClass("btn-secondary");
+});
+
+createForm(products);
